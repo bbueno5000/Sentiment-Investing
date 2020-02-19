4b37c2297c4cad8baca2cb6f8128789826b7ad34
B. Bueno <bbueno25@hotmail.com>
2020-02-19 09:42:48 -0700

    Python and Pandas for Sentiment Analysis and Investing 3 - Looking at our Data

--- sentiment_analysis_and_investing/src/sentiment_analysis_and_investing.py
+++ sentiment_analysis_and_investing/src/sentiment_analysis_and_investing.py
@@ -9,34 +9,37 @@ from matplotlib import style
 
 style.use('ggplot')
 
-sp500 = pandas_datareader.data.get_data_yahoo(
-    '%5EGSPC', 
-    start=datetime.datetime(2000, 10, 1),
-    end=datetime.datetime(2012, 1, 1)
-    )
+def introduction():
+    """
+    DOCSTRING
+    """
+    sp500 = pandas_datareader.data.get_data_yahoo(
+        '%5EGSPC', 
+        start=datetime.datetime(2000, 10, 1),
+        end=datetime.datetime(2012, 1, 1)
+        )
+    sp500.to_csv('sp500_ohlc.csv')
+    dataframe_a = pandas.read_csv('sp500_ohlc.csv', index_col='Date', parse_dates=True)
+    dataframe_a['high_minus_low'] = dataframe_a.High - dataframe_a.Low
+    close = dataframe_a['Adj Close']
+    moving_average = close.rolling(50).mean()
+    axis_1 = pyplot.subplot(2, 1, 1)
+    axis_1.plot(close, label='sp500')
+    axis_1.plot(moving_average, label='ma50')
+    pyplot.legend()
+    axis_2 = pyplot.subplot(2, 1, 2, sharex=axis_1)
+    axis_2.plot(dataframe_a['high_minus_low'], label='H-L')
+    pyplot.legend()
+    pyplot.show()
 
-sp500.to_csv('sp500_ohlc.csv')
+def modify_dataset():
+    """
+    DOCSTRING
+    """
+    dataframe_a = pandas.read_csv('data/stocks_sentdex_1-6-2016.csv')
+    dataframe_a['time'] = pandas.to_datetime(dataframe_a['time'], unit='s')
+    dataframe_a = dataframe_a.set_index('time')
+    dataframe_a.to_csv('data/stocks_sentdex_1-6-2016_full.csv')
 
-dataframe_a = pandas.read_csv('sp500_ohlc.csv', index_col='Date', parse_dates=True)
-
-dataframe_a['high_minus_low'] = dataframe_a.High - dataframe_a.Low
-
-close = dataframe_a['Adj Close']
-
-moving_average = close.rolling(50).mean()
-
-axis_1 = pyplot.subplot(2, 1, 1)
-
-axis_1.plot(close, label='sp500')
-
-axis_1.plot(moving_average, label='ma50')
-
-pyplot.legend()
-
-axis_2 = pyplot.subplot(2, 1, 2, sharex=axis_1)
-
-axis_2.plot(dataframe_a['high_minus_low'], label='H-L')
-
-pyplot.legend()
-
-pyplot.show()
+if __name__ == '__main__':
+    modify_dataset()
